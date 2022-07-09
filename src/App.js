e48047bd65cb5026ca724a32b9d1239654b9d9f2
lupusalba <a145296@gmail.com>
2022-07-09 12:40:49 +0200

    50%, missing avreage bg color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// API KEY = bFNqkMyCnMATKhSuyIfHmmhqUNF7gVnJNwo2Loha
+// https://api.nasa.gov/planetary/apod?api_key=bFNqkMyCnMATKhSuyIfHmmhqUNF7gVnJNwo2Loha
+
+// fetch("https://api.nasa.gov/planetary/apod?api_key=bFNqkMyCnMATKhSuyIfHmmhqUNF7gVnJNwo2Loha")
+//         .then(response => response.json())
+//        .then(json => {
+//         console.log(json.copyright)
+//     })
+
+
 function App() {
+
+  const api = {
+    base: "https://api.nasa.gov/planetary/apod?api_key=",
+    key: "bFNqkMyCnMATKhSuyIfHmmhqUNF7gVnJNwo2Loha"
+  }
+
+  const [apodData, setApodData] = useState({})
+
+
+
+  useEffect( () => {
+    const getImage = () => {
+      try {
+        fetch(`${api.base}${api.key}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            setApodData(json)
+          })
+        console.log(`${api.base}${api.key}`)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getImage()
+  }, [])
+
+
+  const background = {
+    backgroundImage: 'url(' + apodData.hdurl + ')'
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      
+      <div className="main" style={background} >
+
+        <div className="header">
+        <h1 className="siteTitle">Nasa's apod api app</h1>
+          <form>
+            <label className="label" htmlFor="datePicker">Pick a date:</label>
+            <input
+              type="date"
+              id="datePicker"
+              className="datePicker"
+              //name={datePicker}
+              //onChange={handelChange}
+            />
+          </form>
+        </div>
+
+        <div className="descriptionInfo">
+          <div className="imageTitle">{apodData.title}</div>
+          <div className="imageDate">{apodData.date}</div>
+          <div className="imageAuthor">{apodData.copyright}</div>
+          <div className="imageDescription">{apodData.explanation}</div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+      </div>
+
+
     </div>
   );
 }
